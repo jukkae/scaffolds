ec062d9208126537585b984f74b3662be9ea9513
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2016-09-22 11:57:35 +0300

    add powerups

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -13,16 +13,20 @@ public class BallController : MonoBehaviour {
 
 	public Text winText;
 
-	private float groundDistance = .5f;
-
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
 		winText.text = "";
-		rb.maxAngularVelocity = 50;
+		rb.maxAngularVelocity = 1000;
 	}
 
 	bool IsGrounded ()
 	{
+		// TODO dirty; move to some sort of scale variable
+		float groundDistance = GetComponent<SphereCollider> ().radius * transform.localScale.x;
+		// TODO doesn't work if hanging between two edges, move to spherecast
+		// http://answers.unity3d.com/questions/637836/checking-if-grounded-using-spherecast-acting-like.html
+		// -> nice puzzle: two rails, need to grow/shrink the ball to a certain size so that the ball rides
+		// between the rails and gets stuff from below the rails
 		return Physics.Raycast (transform.position, - Vector3.up, groundDistance + 0.1f);
 	}
 	
@@ -39,8 +43,6 @@ public class BallController : MonoBehaviour {
 	void Update () {
 		if (IsGrounded ()) {
 			if (Input.GetKey (KeyCode.Space)) {
-				//Vector3 jump = new Vector3 (.0f, jumpspeed, .0f);
-				//rb.AddForce (jump, ForceMode.Impulse);
 				if (jmp < jumpspeed) {
 					jmp += .5f;
 				}
@@ -59,6 +61,20 @@ public class BallController : MonoBehaviour {
 		}
 		if (other.gameObject.CompareTag ("Boundary")) {
 			winText.text = "YOU LOSE";
+		}
+		if (other.gameObject.CompareTag ("SmallerPowerup")) {
+			transform.localScale = new Vector3 (.5f * transform.localScale.x,
+												.5f * transform.localScale.y,
+												.5f * transform.localScale.z);
+			rb.mass *= .5f;
+			other.gameObject.SetActive (false);
+		}
+		if (other.gameObject.CompareTag ("LargerPowerup")) {
+			transform.localScale = new Vector3 (2.0f * transform.localScale.x,
+												2.0f * transform.localScale.y,
+												2.0f * transform.localScale.z);
+			rb.mass *= 2.0f;
+			other.gameObject.SetActive (false);
 		}
 	}
 }
